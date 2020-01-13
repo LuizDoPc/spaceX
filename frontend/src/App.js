fc71c6b238a4335e2d313ee158a6008d920f9a9c
luizdopc <luizotaviosoares@outlook.com>
2020-01-13 02:40:04 -0300

    start frontend and add cors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+import Launches from './components/Launches';
+
+import './App.css';
+
+import logo from './logo.png';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+});
 
 const App = () => {
-  return <div>Hello React</div>;
+  return (
+    <ApolloProvider client={client}>
+      <div className='app'>
+        <img src={logo} className='logo' alt='spacex' />
+        <Launches />
+      </div>
+    </ApolloProvider>
+  );
 };
 
 export default App;
